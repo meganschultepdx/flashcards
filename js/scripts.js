4966a178e6c1aff9788b69f8713b925e9f070603
Kaya Jepson and Megan Schulte <kaya@>
2019-03-26 15:09:39 -0700

    add initial files and info

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $(".showing-1").toggle();
+    $(".hidden-1").toggle();
+});
+
+  $(".clickable2").click(function() {
+    $(".showing-2").toggle();
+    $(".hidden-2").toggle();
+  });
+
+  $(".clickable3").click(function() {
+    $(".showing-3").toggle();
+    $(".hidden-3").toggle();
+  });
+});
