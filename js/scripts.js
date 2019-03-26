ac68eea7f3fcfcfe845de47fad72b09f77904374
Kaya Jepson and Megan Schulte <kaya@>
2019-03-26 16:49:15 -0700

    add additional flashcards with jQuery functions

--- js/scripts.js
+++ js/scripts.js
@@ -13,4 +13,24 @@ $(document).ready(function() {
     $(".showing-3").toggle();
     $(".hidden-3").toggle();
   });
+
+  $(".clickable4").click(function() {
+    $(".showing-4").toggle();
+    $(".hidden-4").toggle();
+  });
+
+  $(".clickable5").click(function() {
+    $(".showing-5").toggle();
+    $(".hidden-5").toggle();
+  });
+
+  $(".clickable6").click(function() {
+    $(".showing-6").toggle();
+    $(".hidden-6").toggle();
+  });
+
+  $(".clickable7").click(function() {
+    $(".showing-7").toggle();
+    $(".hidden-7").toggle();
+  });
 });
